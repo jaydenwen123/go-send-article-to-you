e1821398a505de425665719b44c9b87d524cb31f
jaydenwen <jaydenwen@tencent.com>
2020-03-05 14:44:23 +0800

    feat:finish read config info

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type ConfigInfo struct {
-	CategoryDataSources []*CategoryDataSources `json:"category_data_sources"`
+	CategoryDataSources []CategoryDataSources `json:"category_data_sources"`
 }
 type CategoryDataSources struct {
 	PageURL          string `json:"page_url"`
--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-	"github.com/jaydenwen123/go-send-article-to-you/config"
 	"github.com/astaxie/beego/logs"
+	"github.com/jaydenwen123/go-send-article-to-you/config"
 	"github.com/jaydenwen123/go-util"
 )
 
 var (
-	//1.要爬取的网页链接
-	url = "http://blog.studygolang.com/"
-	//定位到目录的那一层超链接选择器
-	sector = ".primary-menu  a"
+//1.要爬取的网页链接
+/*url = "http://blog.studygolang.com/"
+//定位到目录的那一层超链接选择器
+sector = ".primary-menu  a"*/
 )
 
 //配置信息
-var configInfo = &config.ConfigInfo{}
+var configInfo = config.ConfigInfo{}
 
 func init() {
-	util.LoadObjectFromJsonFile("./config/config.json", configInfo)
+
+	util.LoadObjectFromJsonFile("go-send-article-to-you/config/config.json", &configInfo)
 	logs.Debug("load the config info success...")
-	logs.Debug("the config info:%v", configInfo)
+	logs.Debug("the config info:%+v", configInfo)
 }
 
 func main() {
 
-	list := GetCategoryList(url, sector)
-	for _, category := range list {
-		logs.Debug("%+v", category)
+	for _, item := range configInfo.CategoryDataSources {
+		list := GetCategoryList(item.PageURL, item.CategorySelector)
+		for _, category := range list {
+			logs.Debug("%+v", category)
+		}
 	}
 }
