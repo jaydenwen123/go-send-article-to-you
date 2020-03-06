964435c3b8a01c331dc6efcb88ed65955ecda63a
jaydenwen <jaydenwen@tencent.com>
2020-03-06 14:29:04 +0800

    feat:update cofig path

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 )
 
 var (
-//1.要爬取的网页链接
-/*url = "http://blog.studygolang.com/"
-//定位到目录的那一层超链接选择器
-sector = ".primary-menu  a"*/
+/*
+   //1.要爬取的网页链接
+   url = "http://blog.studygolang.com/"
+   //定位到目录的那一层超链接选择器
+   sector = ".primary-menu  a"
+*/
 )
 
 //配置信息
@@ -18,13 +20,12 @@ var configInfo = config.ConfigInfo{}
 
 func init() {
 
-	util.LoadObjectFromJsonFile("go-send-article-to-you/config/config.json", &configInfo)
+	util.LoadObjectFromJsonFile("config/config.json", &configInfo)
 	logs.Debug("load the config info success...")
 	logs.Debug("the config info:%+v", configInfo)
 }
 
 func main() {
-
 	for _, item := range configInfo.CategoryDataSources {
 		list := GetCategoryList(item.PageURL, item.CategorySelector)
 		for _, category := range list {
