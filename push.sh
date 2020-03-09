c77b6da64ddb9c4053a77f981cb4de10e6798b84
jaydenwen <jaydenwen@tencent.com>
2020-03-09 11:05:23 +0800

    feat:update push.sh again

--- push.sh
+++ push.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 if [ $# == 0 ]; then
   echo "the commit info is empty..."
   exit
 fi
 echo "begin to commit to repositry..."
-echo "commit script:<git commit -a -m $1>"
-git commit -a -m $1
+echo "commit script:<git commit -a -m '$*'>"
+git commit -a -m "$*"
 echo "commit success...."
 echo "begin to push to remote repositry..."
 git push origin master
